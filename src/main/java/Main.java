2167fb83d4aa3d96f96485b662a31381459a5faf
Jhudeska Ignacia <jvsignacia@live.nl>
2026-04-29 01:44:13 +0200

    Added constructor with all asked properties

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args){
 
-        Car myCar = new Car();
+        Car myCar = new Car("Volvo", "Aygo", 2001,"geel", 1.2);
 
         System.out.println(myCar.brand);
         myCar.brand = "Volvo";
