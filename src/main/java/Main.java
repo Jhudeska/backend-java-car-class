e9f7de258caacbcafae224b45257757824a93a24
Jhudeska Ignacia <jvsignacia@live.nl>
2026-04-29 01:38:10 +0200

    Overide the value of the brand property and run the unit test successfully

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args){
 
         Car myCar = new Car();
+
+        System.out.println(myCar.brand);
+        myCar.brand = "Volvo";
         System.out.println(myCar.brand);
     }
 }
